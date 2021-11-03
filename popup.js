ac8d70baa3ba4b1f33321d39cb74bdb0cd6db22d
harryalbert <harryalbert364@gmail.com>
2021-11-03 12:25:06 -0400

    small styling changes

--- popup.js
+++ popup.js
@@ -29,4 +29,15 @@ function main() {
 	});
 }
 
+chrome.browserAction.onClicked.addListener(function() {
+	console.log('hi');
+    var w = 440;
+    var h = 220;
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2); 
+
+    chrome.windows.create({'url': 'redirect.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
+    });
+});
+
 main();
