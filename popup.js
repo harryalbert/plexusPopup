2cacc2455204dbeab9a186ffedc0d75797a80ecc
harryalbert <harryalbert364@gmail.com>
2021-11-04 10:47:50 -0400

    stylistic changes

--- popup.js
+++ popup.js
@@ -27,17 +27,7 @@ function main() {
 	chrome.storage.sync.get(["notes"], function (items) {
 		loadQuill(items.notes);
 	});
+	document.addEventListener("keydown", () => {console.log('hi')});
 }
-
-chrome.browserAction.onClicked.addListener(function() {
-	console.log('hi');
-    var w = 440;
-    var h = 220;
-    var left = (screen.width/2)-(w/2);
-    var top = (screen.height/2)-(h/2); 
-
-    chrome.windows.create({'url': 'redirect.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
-    });
-});
 
 main();
