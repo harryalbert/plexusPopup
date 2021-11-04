36836606dad96d245b35b7a427604115b47edbfe
harryalbert <harryalbert364@gmail.com>
2021-11-04 11:49:16 -0400

    stylistic changes, added more refined auto-saver

--- popup.js
+++ popup.js
@@ -16,18 +16,25 @@ function loadQuill(notes) {
 	});
 
 	if (notes) quill.insertText(0, notes.trim());
+}
 
-	quill.on("text-change", () => {
-		saveNotes(quill.getText());
+function loadListeners() {
+	quill.root.addEventListener("keydown", (e) => {
+		if (e.code == "Enter" || e.code == "Space") saveNotes(quill.getText());
 	});
+
+	//save text every five seconds
+	window.setInterval(() => {
+		saveNotes(quill.getText());
+	}, 5000);
 }
 
 //load stored notes (if they exist) then load quill editor with them
 function main() {
 	chrome.storage.sync.get(["notes"], function (items) {
 		loadQuill(items.notes);
+		loadListeners();
 	});
-	document.addEventListener("keydown", () => {console.log('hi')});
 }
 
 main();
