d8ec7d771e5935bd74718b70571266d06bdce7d7
harryalbert <harryalbert364@gmail.com>
2021-11-08 22:33:28 -0500

    stylistic changes

--- js/quillMethods.js
+++ js/quillMethods.js
@@ -13,8 +13,29 @@ function loadQuill(note) {
 	if (note) quill.insertText(0, note.trim());
 }
 
-const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const days = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 function loadStoredNotes(notes) {
 	let div = document.getElementById("storedNotes");
 	let currentDate = new Date();
@@ -27,8 +48,15 @@ function loadStoredNotes(notes) {
 		//get formated date
 		let dateString = "";
 		let date = toDateTime(key);
-		if (date.toDateString() == currentDate.toDateString()) dateString = formatAMPM(date);
-		else dateString = days[date.getDay()] + " " + months[date.getMonth()] + " " + date.getDate();
+		if (date.toDateString() == currentDate.toDateString())
+			dateString = formatAMPM(date);
+		else
+			dateString =
+				days[date.getDay()] +
+				" " +
+				months[date.getMonth()] +
+				" " +
+				date.getDate();
 
 		//display date as title
 		let title = document.createElement("h1");
@@ -41,9 +69,11 @@ function loadStoredNotes(notes) {
 		newEditor.id = key;
 		div.appendChild(newEditor);
 
-		let horizontalLine = document.createElement("hr");
-		horizontalLine.className = "horizontalLine";
-		div.appendChild(horizontalLine)
+		if (i > 0) {
+			let horizontalLine = document.createElement("hr");
+			horizontalLine.className = "horizontalLine";
+			div.appendChild(horizontalLine);
+		}
 
 		//create new quill editor within div
 		let storedNote = new Quill(document.getElementById(key), {
@@ -65,8 +95,8 @@ function loadStoredNotes(notes) {
 function toDateTime(secs) {
 	var t = new Date(1970, 0, 1); // Epoch
 	t.setSeconds(secs);
-	t.setDate(t.getDate()-1);
-	t.setHours(t.getHours()+19);
+	t.setDate(t.getDate() - 1);
+	t.setHours(t.getHours() + 19);
 	return t;
 }
 
@@ -74,10 +104,10 @@ function toDateTime(secs) {
 function formatAMPM(date) {
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
-	var ampm = hours >= 12 ? 'pm' : 'am';
+	var ampm = hours >= 12 ? "pm" : "am";
 	hours = hours % 12;
 	hours = hours ? hours : 12; // the hour '0' should be '12'
-	minutes = minutes < 10 ? '0'+minutes : minutes;
-	var strTime = hours + ':' + minutes + ' ' + ampm;
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	var strTime = hours + ":" + minutes + " " + ampm;
 	return strTime;
-  }
+}
